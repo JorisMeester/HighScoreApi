6d06978bf90356b357bdd6be6f1a674464925927
JorisMeester <jorismeester98@gmail.com>
2018-09-26 23:51:44 +0200

    meeste sjit

---
+++ HighScoreApi/Controllers/HighScoresController.cs
@@ -0,0 +1,34 @@
+﻿using HighScoreApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        HighScore[] highScores = new HighScore[]
+        {
+            new HighScore { Id = 1, Score = 1000, UserName = "Groceries" },
+            new HighScore { Id = 2, Score = 100, UserName = "Groceries" },
+            new HighScore { Id = 3, Score = 10, UserName = "Groceries" }
+        };
+
+        public IEnumerable<HighScore> GetAllHighScores()
+        {
+            return highScores;
+        }
+
+        public IHttpActionResult GetHighScore(int id)
+        {
+            var highScore = highScores.FirstOrDefault((p) => p.Id == id);
+            if (highScore == null)
+            {
+                return NotFound();
+            }
+            return Ok(highScore);
+        }
+    }
+}
---
+++ HighScoreApi/Models/HighScore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HighScoreApi.Models
+{
+    public class HighScore
+    {
+        public int Id { get; set; }
+        public int Score { get; set; }
+        public string UserName { get; set; }
+    }
+}
