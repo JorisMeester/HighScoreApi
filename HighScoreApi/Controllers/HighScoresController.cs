14fa956b361f53b050025fe034cb602808fea129
JorisMeester <jorismeester98@gmail.com>
2018-09-27 12:36:35 +0200

    Werkt op laptop

--- HighScoreApi/Controllers/HighScoresController.cs
+++ HighScoreApi/Controllers/HighScoresController.cs
@@ -5,30 +5,30 @@ using System.Linq;
 using System.Net;
 using System.Web.Http;
 
-namespace ProductsApp.Controllers
+namespace HighScoreApi.Controllers
 {
-    public class ProductsController : ApiController
+    public class HighScoresController : ApiController
     {
-        HighScore[] highScores = new HighScore[]
+        HighScore[] highscores = new HighScore[]
         {
-            new HighScore { Id = 1, Score = 1000, UserName = "Groceries" },
-            new HighScore { Id = 2, Score = 100, UserName = "Groceries" },
-            new HighScore { Id = 3, Score = 10, UserName = "Groceries" }
+            new HighScore { Id = 1, Score = 1000, UserName = "Henk" },
+            new HighScore { Id = 2, Score = 100, UserName = "Klaas" },
+            new HighScore { Id = 3, Score = 10, UserName = "Piet" }
         };
 
         public IEnumerable<HighScore> GetAllHighScores()
         {
-            return highScores;
+            return highscores;
         }
 
         public IHttpActionResult GetHighScore(int id)
         {
-            var highScore = highScores.FirstOrDefault((p) => p.Id == id);
-            if (highScore == null)
+            var highscore = highscores.FirstOrDefault((p) => p.Id == id);
+            if (highscore == null)
             {
                 return NotFound();
             }
-            return Ok(highScore);
+            return Ok(highscore);
         }
     }
 }
